34070e9db332c348d1a3d79fc48cba2c91efd8ae
zack8361 <zack6353@naver.com>
2023-04-21 14:59:44 +0900

    eslint , vscode 세팅
    eslint , vscode 세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            LEEEEE HANAAAAAAA
-            <br />
-            HWANG BU SANG DDA BONG !!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">hello chanho yoongu heejin hana</header>
     </div>
   );
 }
