2242749d5bfab480e30304418ced7e1fdf1d0063
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-05-14 19:05:58 +0900

    Mypage 오ㅏㄴㅅㅓㅇ

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import "../style/mypage.scss";
-import base from "../images/base.png";
-import { useRef } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import crypto, { HmacSHA256, SHA256 } from "crypto-js";
-import { useState } from "react";
 
 export default function Mypage() {
 
@@ -26,6 +20,30 @@ export default function Mypage() {
         </div>
         <div className="right_container">
           <div className="text_box1"></div>
+          <div className="text_box2"></div>
+          <div className="text_box3"></div>
+          <div className="text_line1"></div>
+          <div className="text_line2"></div>
+          <div className="category_box1">소통</div>
+          <button className="communi_btn1">
+            1. 쪽지보내기
+          </button>
+          <button className="communi_btn2">
+            2. 내가보낸쪽지보기
+          </button>
+          <button className="communi_btn3">
+            3. 내가받은쪽지보기
+          </button>
+          <div className="category_box2">계정</div>
+          <button className="account_btn1">
+            1. 로그아웃하기
+          </button>
+          <button className="account_btn2">
+            2. 비밀번호변경하기
+          </button>
+          <button className="account_btn3">
+            3. 회원탈퇴하기
+          </button>
         </div>
       </div>
     </>
