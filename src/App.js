993483dfcdd70695262243978fa8e43c8c26d0ca
zack8361 <zack6353@naver.com>
2023-05-29 02:29:31 +0900

    설정 완료
    설정 완료

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from "react";
 import "./firebase";
-import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
-import Register from "./pages/Register";
-import Main from "./pages/Main";
-import Mypage from "./pages/Mypage";
-import Textpage from "./pages/Textpage";
-import Emailauth from "./pages/Emailauth";
-import Authfail from "./pages/Authfail";
-import Authsuc from "./pages/Authsuc";
-import Sendtxt from "./pages/SendText";
-import Recvtxt from "./pages/Recvtxt";
 import "./index.css";
 import Integrated from "./pages/Integrated";
 // 전체 를 불러오는 부분인데.
