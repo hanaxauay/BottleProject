539eae6b2e4c4456f235ed2ba3e89c6ed0fe5d55
zack8361 <zack6353@naver.com>
2023-07-31 21:59:33 +0900

    app.js
    app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Integrated from "./pages/Integrated";
 import Main from "./pages/Main";
+import Mypage from "./pages/Mypage";
 // 전체 를 불러오는 부분인데.
 function App() {
+  // 여기 안에 Main 을 지우고 희진이와 영신이가 만든 jsx 파일을 import 해서 main과 같은 형식으로 넣으면됩니다.
   return (
     <>
       <Main />
