27bcc869743936a52b136f12e9393f0a35c9160b
seoyoungshinn <doddo1225@gmail.com>
2023-08-07 01:44:39 +0900

    recvList 대강완성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './firebase';
-import { Route, Routes } from 'react-router-dom';
-import './index.css';
+import React from "react";
+import "./firebase";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import Integrated from "./pages/Integrated";
+import Main from "./pages/Main";
+import Mypage from "./pages/Mypage";
 
-import Main from './pages/Main';
+import MRecvList from "./pages/m-RecvList";
 
 // 전체 를 불러오는 부분인데.
 function App() {
   // 여기 안에 Main 을 지우고 희진이와 영신이가 만든 jsx 파일을 import 해서 main과 같은 형식으로 넣으면됩니다.
   return (
     <>
-      <Main />
+      <MRecvList/>
     </>
   );
 }
--- src/pages/m-RecvList.jsx
+++ src/pages/m-RecvList.jsx
@@ -19,22 +19,38 @@ const MRecvList = () => {
                     <div className="top">
                         [내가 받은 쪽지함]
                     </div>  {/*end of  top*/}
-                    <div className="mid"> {/*용준->여기안에다가 디브로 두개 구분해서 넣은다음에 scss에서 작업하면댐*/}
+                    <div className="mid"> 
                         
-  
+                    <table>
+                        <tr>
+                        <th>[번호]</th>
+                        <th>제목</th>
+                        </tr>
+                        <tr>
+                        <td>[1]</td>
+                        <td>이거어케불러올거임</td>
+                        </tr>
+                        <tr>
+                        <td>[2]</td>
+                        <td>데이터 셀 2</td>
+                        </tr>
+                        <tr>
+                        <td>[3]</td>
+                        <td>데이터 셀 2</td>
+                        </tr>
+                        <tr>
+                        <td>[4]</td>
+                        <td>데이터 셀 2</td>
+                        </tr>
+                    
+  </table>
                     </div>  {/*end of mid */}
                     <div className="bottom">
                         <div className="left" onClick={btn_goback}>
-                              <br></br> &nbsp;↜&nbsp; 뒤로가기
+                              &nbsp;↜&nbsp; 뒤로가기
                         </div>  {/*end of left */}
-
-                       { /*여기에 디브 가운테 태그 하나 더 넣고 작업하삼.
-                         onclick은 이 jsx 제일 위에 function해놓은거 똑같은 형식으로 만들어서 추가하삼
-                       
-                       */}
-
                         <div className="right" onClick={btn_sendlist}>
-                             <br></br>  내가 쓴 쪽지함 &nbsp;➣&nbsp;
+                              내가 쓴 쪽지함 &nbsp;➣&nbsp;
                         </div>  {/*end of right */}
 
                     </div>  {/*end of bottom */}
