6a664b214827a9ebf3ecc38a5134bf54055e6569
seoyoungshinn <doddo1225@gmail.com>
2023-08-12 23:58:34 +0900

    settings fin

--- src/pages/TEMP.jsx
+++ src/pages/TEMP.jsx
@@ -27,29 +27,26 @@ function TEMP() {
     <div className="bg">
         <div className = "Right">
             <div className ="Container">
-
-                <div className="mid">
-                    <div className="email">
-                        <br></br>
-                        {email}
-                    </div>
-                    <div className="info">
-                        <br></br>
-                        ┌〓〓〓〓〓<span style={{ color: '#850000' }}>♡</span>〓〓〓〓〓┐<br></br>
-                        ҉ ٩(๑{'>'}ω{'<'}๑)۶҉ <br></br>
-                        방フГ방フГ しΗ친구~!<br></br>
-                        <span style={{ color: '#850000' }}>{dateText}</span>ના フГ입ṑΗᨈㅓ<br></br>
-                        벌써 ㅎ占께ㅎŁスı <br></br>
-                        ♡ D + <span style={{ color: '#850000' }}>{daysText}</span> 일 ♡ 째긔<br></br>
-                        ユ 동ㅇビ Ꮣㅓ는 <br></br>
-                        ♥ <span style={{ color: '#850000' }}>{sentMessages}</span>통 ♥의 편スı를 보냈ヱ<br></br>
-                        ♥ <span style={{ color: '#850000' }}>{receivedMessages}</span>통 ♥의 편スı를 받았Ꙕ <br></br>
-                        ｡｡ ᕕ༼✿•̀︿•́༽ᕗ<br></br>
-                        앞으로도 우ય 추억 <br></br>
-                        많๐ㅣ 쌓ㅈr ε=٩(●❛ö❛)۶<br></br>
-                        <br></br>
-                        └〓〓〓〓〓<span style={{ color: '#850000' }}>♡</span>〓〓〓〓〓┘<br></br>
-                        <br></br>  
+                <div className="email">
+                    <br></br>
+                    {email}
+                </div>
+                <div className="info">
+                <br></br><br></br><br></br>
+                    ┌〓〓〓〓〓<span style={{ color: '#FF0000' }}>♡</span>〓〓〓〓〓┐<br></br>
+                    ҉ ٩(๑{'>'}ω{'<'}๑)۶҉ <br></br>
+                    방フГ방フГ しΗ친구~!<br></br>
+                    <span style={{ color: '#FF0000' }}>{dateText}</span>ના フГ입ṑΗᨈㅓ<br></br>
+                    벌써 ㅎ占께ㅎŁスı <br></br>
+                    ♡ D + <span style={{ color: '#FF0000' }}>{daysText}</span> 일 ♡ 째긔<br></br>
+                    ユ 동ㅇビ Ꮣㅓ는 <br></br>
+                    ♥ <span style={{ color: '#FF0000' }}>{sentMessages}</span>통 ♥의 편スı를 보냈ヱ<br></br>
+                    ♥ <span style={{ color: '#FF0000' }}>{receivedMessages}</span>통 ♥의 편スı를 받았Ꙕ<br></br>
+                    ｡｡ ᕕ༼✿•̀︿•́༽ᕗ<br></br>
+                    앞으로도 우ય 추억 <br></br>
+                    많๐ㅣ 쌓ㅈr ε=٩(●❛ö❛)۶<br></br>
+                    └〓〓〓〓〓<span style={{ color: '#850000' }}>♡</span>〓〓〓〓〓┘<br></br>
+                    <br></br>  
                 </div>
                 <div className="click_area">
                     <div className="find-pw" onClick={() => alert('비번찾긔@ㅁ@')}>
@@ -59,18 +56,12 @@ function TEMP() {
                         |회원탈퇴|
                     </div>
                     <div className="back-button" onClick={() => alert('뒤로가기 버튼 클릭')}>
-                         |뒤로가기|
+                        |비번변경|
                     </div>
                 </div>
+
             </div>
-        
-
-
-        </div>
-
-
-
-        </div> 
+          </div> 
     </div> /*bg*/
   );
 }
