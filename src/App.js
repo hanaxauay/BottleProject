174e02b5b9bc1c90a08884e03ebc3baae0fb974f
seoyoungshinn <doddo1225@gmail.com>
2023-08-13 00:30:32 +0900

    DeleteAccount PC page _ proto

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./firebase";
 import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Integrated from "./pages/Integrated";
-import TEMP from "./pages/TEMP";
+import TEMP2 from "./pages/TEMP2";
 
 
 // 전체 를 불러오는 부분인데.
@@ -11,7 +11,7 @@ function App() {
   // 여기 안에 Main 을 지우고 희진이와 영신이가 만든 jsx 파일을 import 해서 main과 같은 형식으로 넣으면됩니다.
   return (
     <>
-      <TEMP />
+      <TEMP2 />
     </>
   );
 }
---
+++ src/pages/TEMP2.jsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { useState } from 'react';
+import '../style/TEMP2.scss';
+import RightContainer from '../components/RightContainer';
+
+function TEMP2() {
+
+    const [email, setEmail] = useState('test@hansung.com'); 
+    const [dateText, setDateText] = useState('2023년 4월 6일');
+    const [daysText, setDaysText] = useState('3 4 5');
+    const [sentMessages, setSentMessages] = useState('66');
+    const [receivedMessages, setReceivedMessages] = useState('55');
+  
+    const handleInputChange = (setterFunc, value) => {
+      setterFunc(value);
+    };
+  
+    // 이메일 변경 함수
+    const handleEmailChange = (newEmail) => {
+      setEmail(newEmail);
+    };
+
+
+    
+  return (
+    
+    <div className="bg">
+        <div className = "Right">
+            <div className ="Container">
+                <div className="top">
+                    <br></br>
+                    | 회 원 탈 퇴 |
+                </div>
+                <div className="info">
+                            <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;╱<br></br>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/<br></br>
+                                &nbsp;╲&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;╱<br></br>
+                                &nbsp;&nbsp;╲&nbsp;&nbsp;    설ㅁΓ...&nbsp;&nbsp;&nbsp;╱<br></br>
+                                --&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;진ㅉ&nbsp;ŀ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;-&nbsp;-<br></br>
+                                &nbsp;&nbsp;╱&nbsp;   탈 퇴 ṑトレト요?&nbsp;&nbsp;╲<br></br>
+                                &nbsp;&nbsp;/     (ಥ﹏ಥ)(ಥ﹏ಥ)      .<br></br>
+                                &nbsp;&nbsp;╱&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;╲<br></br>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;|<br></br>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.<br></br>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.<br></br>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.<br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                ┳┻|__∧<br></br>
+                                ┻┳|•﹃ •)  
+                                ┳┻|⊂ﾉ <br></br>
+                                ┻┳|Ｊ┻┳|     
+                                ┳┻|__∧<br></br>
+                                ┻┳|•﹃ •)<br></br>
+                                ┳┻|⊂ﾉ<br></br>
+                                ┻┳|Ｊ<br></br>
+                                우己l ○l제 못봐?<br></br>
+                                (´°̥̥̥̥̥̥̥̥ω°̥̥̥̥̥̥̥̥｀)(´°̥̥̥̥̥̥̥̥ω°̥̥̥̥̥̥̥̥｀)<br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                &nbsp;&nbsp;;ハ＿ハ;+ヾ<br></br>
+                                &nbsp;⊂(+ ｀Д´)つ<br></br>
+                                &nbsp;;/&nbsp;&nbsp;゜。゜。<br></br>
+                                &nbsp;;し￣Ｊ。゜。゜&nbsp;&nbsp;;ハ＿ハ;+ヾ<br></br>
+                                &nbsp;⊂(+ ｀Д´)つ<br></br>
+                                &nbsp;;/&nbsp;&nbsp;゜。゜。<br></br>
+                                &nbsp;;し￣Ｊ。゜。゜<br></br>
+                                <br></br>
+                                ㄴr 없๐ㅣ 잘 ⋌トㄴr 보ㅈr ! 흥<br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                ⠀ ^∧＿∧^              ㅁビㄴr서  즐ᥐㅓ웠Ꙕ<br></br>
+                                ( ´ •̥̥̥ ω •̥̥̥ )&nbsp;&nbsp;  ❀   행복ṑΗ࿁ㅇㅑ ṑΗ<br></br>
+                                ,, ( ヽ∩∩ ) ,, ヽ|〃 ,,,  또 ㅁビㄴrㅈr  ♡<br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <div className="changepwd">
+                    <div className="inputarea">
+                        <div className="pwd">
+                            비밀번호를 입력하세요 &nbsp;&nbsp;
+                            <input className="pwd-input" type="password" placeholder="현재 비밀번호" />
+                        </div>                 
+                    </div>{/*end of inputarea*/}
+                    <input type="button" className="btn_bye" onClick={() => alert('잘가라')} value="&nbsp;탈퇴하기&nbsp;" />
+                </div> {/*end of changepwd*/}
+                
+                
+                </div>
+
+             
+                <div className="bottom">
+                    다시한번 생각해 보긔,,
+                </div>
+            </div>
+        </div> 
+    </div> /*bg*/
+  );
+}
+
+export default TEMP2;
