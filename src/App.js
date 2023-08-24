1da980e079d7c667184b08e86214dadbbc65fb54
heejinkong <gmlwls3379@naver.com>
2023-08-24 20:19:00 +0900

    pc 디자인 수정해봄
    pc 디자인 수정해봄

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './firebase';
-import { Route, Routes } from 'react-router-dom';
-import './index.css';
+import React from "react";
+import "./firebase";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import Main from "./pages/Main";
 
-import Main from './pages/Main';
 //import MobileMain from './pages/MobileMain';//<MobileMain />
 //import MLogin  from './pages/m-Login';
 
@@ -12,8 +12,7 @@ function App() {
   // 여기 안에 Main 을 지우고 희진이와 영신이가 만든 jsx 파일을 import 해서 main과 같은 형식으로 넣으면됩니다.
   return (
     <>
-      
-<Main />
+      <Main />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,30 @@
-import {Link} from "react-router-dom";
-
-
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div className="homeComponent">
-            <div className="category_box1">소개</div>
-            <div className="text_main">~ 우리쪽지는 어쩌공 저쩌공 소개중 ~</div>
-            <div className="category_box2">쪽지시작</div>
-            <button className="login_btn">
-                <Link to="/login" className="login">
-                    1. 로그인하기
-                </Link>
-            </button>
-            <button className="register_btn">
-                <Link to="/register" className="register">
-                    2. 회원가입하기
-                </Link>
-            </button>
-        </div>
-    );
+  return (
+    <div className="homeComponent">
+      <div className="category_box1">소개</div>
+      <div className="text_main">
+        <br />
+        ┍━♫¸.•¨\`♬ʕ ﾉ•ᴥ•ʔﾉ♫ヽʕ•ᴥ•ʔﾉ♩ヽʕ •ᴥ•ʔﾉ♬* .♫━┑
+        <br />
+        ├━♬ 삐 - 쪽지 ♬━┤
+        <br />
+        <br />
+        ┕━━━━━━━━━━━━━━• ♪ ♬ ♪ •━━━━━━━━━━━━━━━━━┙
+      </div>
+
+      <div className="category_box2">쪽지시작</div>
+      <button className="login_btn">
+        <Link to="/login" className="login">
+          1. 로그인하기
+        </Link>
+      </button>
+      <button className="register_btn">
+        <Link to="/register" className="register">
+          2. 회원가입하기
+        </Link>
+      </button>
+    </div>
+  );
 }
